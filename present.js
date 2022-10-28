3878a562ad265a2f2e813a2077aa911ed0a860b1
AlphaBryan <90648570+AlphaBryan@users.noreply.github.com>
2022-10-28 19:30:15 -0400

    Create present.js

---
+++ present.js
@@ -0,0 +1,13 @@
+function Presentation(Linkedin) {
+    console.log('Hello World');
+    console.log('My name is ' + Bryan.Mevo);
+    console.log(`I'm currently doing a Master's Degree in Computer Science ${ETS}, Montreal`);
+    console.log(`I'm currently working as a Software Developer at ${Minstère.de.la.Cybersécurité.du.Québec}`);
+    console.log("On the DevSecOps ROAD !");
+}
+
+export { NodeJs, React, MongoDB, API, Python, JAVA, AWS }
+
+// ============================================> ♥ Let's connect !  <============================================
+
+Presentation('Linkedin');
